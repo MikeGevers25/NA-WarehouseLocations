93e774f158d29daed26ebc9680185be2fdeb7b63
MikeGevers25 <mgevers@apexinternational.com>
2025-07-16 19:27:28 +0200

    Update barcode_gen.py

--- utils/barcode_gen.py
+++ utils/barcode_gen.py
@@ -4,17 +4,17 @@ from barcode.writer import ImageWriter
 import os
 
 def generate_barcode(werkorder, output_dir):
-    # Vervang onveilige tekens in werkorder
-    safe_name = werkorder.replace("/", "-")
-
     # Zorg dat de map bestaat
     os.makedirs(output_dir, exist_ok=True)
 
-    # barcode.save() slaat automatisch op als .png als je ImageWriter gebruikt
-    full_path = os.path.join(output_dir, safe_name)
+    # Maak een veilige bestandsnaam (geen / of andere rare tekens)
+    safe_name = werkorder.replace("/", "-")
+
+    # Pad zonder extensie (wordt .png door barcode.save toegevoegd)
+    full_path_no_ext = os.path.join(output_dir, safe_name)
+
+    # Barcode genereren (Code128) en opslaan als PNG
     code128 = barcode.get("code128", werkorder, writer=ImageWriter())
+    final_path = code128.save(full_path_no_ext)
 
-    # Deze functie voegt zelf .png toe → dus we krijgen het exacte pad terug
-    final_filename = code128.save(full_path)
-
-    return final_filename
+    return final_path  # bijv. 'barcodes/W25-012345.png'
