6d149b59285034fea768a771311c285216c7211e
MikeGevers25 <mgevers@apexinternational.com>
2025-07-16 18:29:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,48 @@
 import streamlit as st
+import pandas as pd
+import os
+import datetime
+from utils.barcode_gen import generate_barcode
 
-st.title("🎉 Test App voor Streamlit Mike Gevers is de beste shit show supervisor ever")
+# Mappen en bestandsnamen
+DATA_PATH = 'data/voorraad.csv'
+BARCODE_DIR = 'barcodes'
+os.makedirs('data', exist_ok=True)
+os.makedirs(BARCODE_DIR, exist_ok=True)
 
-st.write("Als je dit ziet, werkt je app!")
+# Data laden of initialiseren
+def load_data():
+    if os.path.exists(DATA_PATH):
+        return pd.read_csv(DATA_PATH)
+    else:
+        df = pd.DataFrame(columns=["werkorder", "locatie", "status", "datum"])
+        df.to_csv(DATA_PATH, index=False)
+        return df
 
-if st.button("Klik mij als je het ermee eens bent!"):
-    st.success("Ow Yes Mike you are!!.")
+# Data opslaan
+def save_data(df):
+    df.to_csv(DATA_PATH, index=False)
+
+# Streamlit configuratie
+st.set_page_config(page_title="Kisten Voorraad App", layout="wide")
+st.title("💼 Kisten Voorraadbeheer - Anilox")
+
+menu = st.sidebar.radio("Navigatie", ["Nieuwe Binnenkomst", "Zoeken & Beheer"])
+
+df = load_data()
+
+if menu == "Nieuwe Binnenkomst":
+    st.header("Nieuwe kist registreren")
+    werkorder = st.text_input("Werkorder (bijv. W25/012345)")
+    locatie = st.text_input("Locatie (bijv. MAG-A2)")
+
+    if st.button("Registreer kist"):
+        if werkorder and locatie:
+            if werkorder in df['werkorder'].values:
+                st.warning("Deze werkorder bestaat al!")
+            else:
+                barcode_path = generate_barcode(werkorder, BARCODE_DIR)
+                datum = datetime.datetime.now().strftime("%Y-%m-%d")
+                df = pd.concat([df, pd.DataFrame.from_records([{
+                    "werkorder": werkorder,
+                    "loca
